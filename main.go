10fd3223d1c1b00213cd8521f7400aa2744eee1a
Thomas Koefod <devinthemtn@gmail.com>
2025-04-28 15:16:30 -0600

    init

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/charmbracelet/huh"
+)
+
+type Client struct {
+	name    string
+	dirName string
+	path    string
+}
+
+func main() {
+	clientList := [2]Client{
+		{name: "Salish", dirName: "Salish"},
+		{name: "Blackfoot", dirName: "Blackfoot"},
+	}
+
+	var (
+		clientName string
+	)
+	var options []huh.Option[string]
+
+	for _, val := range clientList {
+		options = append(options, huh.Option[string]{Value: val.name, Key: val.name})
+		// fmt.Println(val.name)
+	}
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("chose your client").
+				Options(options...).Value(&clientName),
+		),
+	)
+
+	err := form.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(clientName)
+}
