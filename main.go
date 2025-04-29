25d6d7c708b9470fd8210ef613cad0229ccd2d85
Thomas Koefod <devinthemtn@gmail.com>
2025-04-29 10:59:10 -0600

    getting file path

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/charmbracelet/huh"
 )
@@ -18,8 +19,13 @@ func main() {
 		{name: "Dev", dirName: "Dev"},
 		{name: "Salish", dirName: "Salish"},
 		{name: "Blackfoot", dirName: "Blackfoot"},
+		{name: "Tit", dirName: "Tit"},
+		{name: "Nse", dirName: "Nse"},
 	}
-
+	// var sourceDir string = "mypath"
+	// var destinationDir string = "path"
+	//  sourcePath := filepath.Join("source_dir", "my_file.txt")
+	// destinationPath := filepath.Join("destination_dir", "my_file.txt")
 	var (
 		clientName string
 	)
@@ -27,7 +33,6 @@ func main() {
 
 	for _, val := range clientList {
 		options = append(options, huh.Option[string]{Value: val.name, Key: val.name})
-		// fmt.Println(val.name)
 	}
 	form := huh.NewForm(
 		huh.NewGroup(
@@ -42,4 +47,9 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(clientName)
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	fmt.Println(cwd)
 }
